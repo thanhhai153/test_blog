cb0b73579e801575ca7897d47a83200535cbbc43
ATH <thanhhai_153>
2023-04-10 09:53:05 +0700

    first commit

---
+++ oop/use.php
@@ -0,0 +1,20 @@
+<?php
+namespace OOP;
+class User{
+    public $username;
+    protected $id;
+    private $password;
+
+    public function adduser($newusername, $newpassword1)
+    {
+        $this-> newusername = $username;
+        $this-> newpassword1 = $newpassword1;
+        return getuser();
+    }
+    public function getUser() {
+
+
+    
+
+}
+?>
